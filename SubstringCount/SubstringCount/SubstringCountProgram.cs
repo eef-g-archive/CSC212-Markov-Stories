ec125dbe9cebb87ac9468d24a03eacdd3b269220
Ethan Gray <74423016+KafLad@users.noreply.github.com>
2022-04-01 15:37:32 -0700

    Substring v2

--- SubstringCount/SubstringCount/SubstringCountProgram.cs
+++ SubstringCount/SubstringCount/SubstringCountProgram.cs
@@ -7,20 +7,26 @@ namespace Program
     {
         static public void Main(string[] args)
         {
-            string[] words = {"Hello", "World", "It's", "Ethan", "!" };
             string txt = "Hellothere,It'sEthanhereabouttobringtotheWorldmynewprogram.It'scalledEthan'scode!";
+            int len = 2;
+            string [] keys = new string [txt.Length / len];
             
-            MarkovEntry[] entries = new MarkovEntry[words.Length];
-            for(int i = 0; i < words.Length; i++)
+            int j = 0;
+            for (int i = 0; i < txt.Length; i+=len)
+            {
+                keys[j] = txt.Substring(i, i + len);
+                Console.WriteLine(keys[j]);
+                j++;
+            }
+            MarkovEntry[] entries = new MarkovEntry[keys.Length];
+
+            for(int i = 0; i < keys.Length; i ++)
             {
                 Console.WriteLine($"#== Testing MarkovEntry {i} ==#");
-                entries[i] = new MarkovEntry(words[i]);
-                char[] indexes = entries[i].GetChars(txt);
-                foreach(char j in indexes)
-                {
-                    entries[i].Add(j);
-                }
+                entries[i] = new MarkovEntry(keys[i]);
+                entries[i].ScanText(txt);
                 Console.WriteLine("\n" + entries[i] + "\n");
+                entries[i].PrintDistinctKeys();
             }
         }
     }
--- SubstringCount/SubstringCountLibrary/SubstringCountLibrary.cs
+++ SubstringCount/SubstringCountLibrary/SubstringCountLibrary.cs
@@ -1,10 +1,10 @@
-﻿
-namespace SubstringCountLibrary
+﻿namespace SubstringCountLibrary
 {
     public class MarkovEntry
     {
         private string key;
         private int count;
+        private Dictionary<string, int> distinctKeys = new Dictionary<string, int>();
 
         public MarkovEntry(string key)
         {
@@ -12,12 +12,20 @@ namespace SubstringCountLibrary
             this.count = 0;
         }
         
+        public void ScanText(string text)
+        {
+            char [] suffixes = GetChars(text);
+            foreach(char c in suffixes)
+            {
+                Add(c);
+            }
+        }
 
         /// <summary>
         /// Goes through the given text and locates the character immediately after the MarkovEntry's key value. Adds each character it 
         /// finds to a Linked List in order of how early it appears. Afterwards returns a list of all the characters in the same order they were in.
         /// </summary>
-        public char[] GetChars(string text)
+        private char[] GetChars(string text)
         {
             // Can replace List with MyList if needed, but for now using the C# default list class since it's just easier tbh
             List<char> chList = new List<char>();
@@ -58,12 +66,29 @@ namespace SubstringCountLibrary
         /// Takes a character, ch, and prints it out with both the MarkovEntry key value followed by ch. Adds 1 to the count value of the MarkovEntry
         /// object since the character comes after where the key was located.
         /// </summary>
-        public void Add(char ch)
-        { 
+        private void Add(char ch)
+        {
+            if(distinctKeys.ContainsKey(ch.ToString()))
+            {
+                distinctKeys[ch.ToString()]++;
+            }
+            else
+            {
+                distinctKeys.Add(ch.ToString(), 1);
+            }
             Console.WriteLine($"Character after key '{key}': '{ch}'");
             count++;
         }
 
+        public void PrintDistinctKeys()
+        {
+            Console.WriteLine($"{distinctKeys.Keys.Count} unique keys");
+            foreach(string k in distinctKeys.Keys)
+            {
+                Console.WriteLine($"  {k} ({distinctKeys[k]})");
+            }
+            Console.WriteLine("\n");
+        }
         public override string ToString()
         {
             return $"MarkovEntry '{key}': {count}";
